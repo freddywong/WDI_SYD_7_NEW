a359f6c97503ea5536988d6b3f6084b1df162ac0
Frederick Wong <frederick.kf.wong@gmail.com>
2014-12-16 21:54:04 +1100

    Valid input scenario now works

--- frederick/w06/d02/blog/app/controllers/posts_controller.rb
+++ frederick/w06/d02/blog/app/controllers/posts_controller.rb
@@ -1,15 +1,27 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+	def index
+		@posts = Post.all
+	end
 
-  def show
-    @post = Post.find(params[:id])
-    @comments = Comment.all
-  end
+	def show
+		@post = Post.find(params[:id])
+		@comments = Comment.all
+	end
 
-  def new
-  	@post = Post.new
-  end
-  
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new post_params
+		@post.save
+		redirect_to posts_path(@post) 
+	end
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content, :author)
+	end
+
+
 end
--- frederick/w06/d02/blog/spec/features/editor_adds_a_blog_posts_spec.rb
+++ frederick/w06/d02/blog/spec/features/editor_adds_a_blog_posts_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 feature "EditorAddsABlogPosts", :type => :feature do
 	scenario "valid input" do
 		visit '/'
+
 		click_link 'Add Post'
 		fill_in('Title', with: 'Hello World')
 		fill_in('Content', with: 'Lorem ipsum dolar sit amet')
 		fill_in('Author', with: 'Jane Doe')
-	end
+		click_button('Create Post')
 
-end
+		expect(page).to have_content("Hello World")
+		expect(page).to have_content("Lorem ipsum dolar sit amet")
+		expect(page).to have_content("Jane Doe")
+	end
+	scenario "invalid input" do
+		
+	end
+end	
