4aa6049240f499eba950ea5f9171b792137e11d5
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-19 10:15:30 +1100

    GEt JSON list of statuses and added some seed data

--- frederick/w09/d01/twitter_clone/db/seeds.rb
+++ frederick/w09/d01/twitter_clone/db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating some seed statuses"
+Status.create({
+  username: "Freddy",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem voluptate placeat quis, ratione omnis maxime dicta quas, earum magnam veniam cum aspernatur officiis. Quae in, a laboriosam provident sapiente excepturi."
+  })
+
+Status.create({
+  username: "Daryl",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut magnam debitis nesciunt, odio. Similique consequatur optio, ipsum, inventore at dicta quas explicabo maxime natus omnis praesentium! Ad nam ea sint."
+  })
+
+Status.create({
+  username: "Jimbo",
+  content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur deleniti inventore, officiis quaerat quae incidunt voluptatem perferendis quasi accusamus! Harum possimus ipsa, reprehenderit quis quo blanditiis vero enim explicabo quam."
+  })
