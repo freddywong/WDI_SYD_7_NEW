ae345f0c84b20142255a82392dc8ba55311622d3
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-19 14:28:42 +1100

    Added some seed data

--- frederick/w09/d01/learnable/db/seeds.rb
+++ frederick/w09/d01/learnable/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+School.create({
+  name: 'General Assembly',
+  description: "Learn technology, design, and business skills from industry professionals in our global community."
+  })
+
+School.create({
+  name: 'UTS',
+  description: "Located in Sydney’s creative industries precinct and close to inner city cultural hubs, our City campus is within easy walking distance of Central Station and the CBD."
+  })
+
+School.create({
+  name: 'MIT',
+  description: "The mission of MIT is to advance knowledge and educate students in science, technology and other areas of scholarship that will best serve the nation and the world in the 21st century."
+  })
