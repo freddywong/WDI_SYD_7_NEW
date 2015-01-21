14160b96a10016f5bf979105339a4f16f804a1b0
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-21 11:38:48 +1100

    Status view no longer needs to be rendered out in timeline.hbs

--- frederick/w09/d01/twitter_clone/app/assets/javascripts/application.js
+++ frederick/w09/d01/twitter_clone/app/assets/javascripts/application.js
@@ -19,3 +19,7 @@
 //= require_tree ./views
 //= require_tree .
 
+var statusesCollection = new Statuses();
+var timelineView = new TimelineView({ collection: statusesCollection});
+
+$("body").append(timelineView.el);
--- frederick/w09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
+++ frederick/w09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
@@ -1,5 +1,19 @@
 var TimelineView = Backbone.View.extend({
   tagName: "div",
-  className: "timeline"
+  className: "timeline",
 
+  template: JST['statuses/timeline'],
+
+  initialize: function() {
+    this.collection.fetch();
+    this.collection.on("sync", this.render, this)
+  },
+
+  render: function() {
+    this.$el.html(
+      this.template({ 
+        statuses: this.collection.toJSON() 
+      })
+    );
+  }
 });
