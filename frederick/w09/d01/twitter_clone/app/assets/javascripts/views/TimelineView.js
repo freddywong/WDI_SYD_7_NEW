ed1d35434e30a1eacc6888683aacbae258500d8e
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-21 14:08:55 +1100

    Completed - Status can now be removed

--- frederick/w09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
+++ frederick/w09/d01/twitter_clone/app/assets/javascripts/views/TimelineView.js
@@ -2,18 +2,13 @@ var TimelineView = Backbone.View.extend({
   tagName: "div",
   className: "timeline",
 
-  template: JST['statuses/timeline'],
-
   initialize: function() {
     this.collection.fetch();
-    this.collection.on("sync", this.render, this)
+    this.collection.on("add", this.addStatus, this)
   },
 
-  render: function() {
-    this.$el.html(
-      this.template({ 
-        statuses: this.collection.toJSON() 
-      })
-    );
+  addStatus: function(status) {
+    var statusView = new StatusView({ model: status });
+    this.$el.append(statusView.render().el);
   }
 });
