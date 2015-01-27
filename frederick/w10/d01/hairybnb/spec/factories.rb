8e3f0a5f2bd0374d47b3696543e9f58d4b4c0d89
Frederick Wong <frederick.kf.wong@gmail.com>
2015-01-27 17:03:04 +1100

    Feature: looking at apartments; Scenario: When there are apartments

---
+++ frederick/w10/d01/hairybnb/spec/factories.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+
+  factory :apartment do
+    sequence(:title) { |n| "Spacious #{n} Bed Apartment with a Grand View" }
+    sequence(:bedrooms) { |n| n }
+    price_in_cents 1_500_000
+    location "Bondi, NSW"
+    description "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam repellat labore in laudantium, quidem totam id nemo, asperiores ad, saepe inventore minus distinctio voluptatum, quas similique? Veritatis ipsa blanditiis laudantium?"
+    available true
+  end
+end
